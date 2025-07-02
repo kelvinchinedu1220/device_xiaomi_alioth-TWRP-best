f26e2b44985137e3eb5138e44cd65f5b9d56d7c9
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-02 20:40:50 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,10 @@
 # limitations under the License.
 #
 
+# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
