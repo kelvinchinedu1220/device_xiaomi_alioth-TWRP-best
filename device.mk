241bc3fcc5a7a59d968c1e6f7bca2255ac287d36
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-03 19:34:52 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,35 +1,35 @@
 #
-# Copyright (C) 2020 The TwrpBuilder Open-Source Project
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/xiaomi/alioth
 
-PRODUCT_PACKAGES += \
-    bootctrl.xiaomi_sm8250.recovery \
-    android.hardware.boot@1.1-impl-qti.recovery
+# API
+PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_SHIPPING_API_LEVEL := 31
 
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 30
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 31
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
+# Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# A/B
+ENABLE_VIRTUAL_AB := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+# BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    system_ext \
+    product \
+    vendor \
+    vendor_boot \
+    odm \
+    vbmeta \
+    vbmeta_system \
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
@@ -38,30 +38,71 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# PRODUCT_RELEASE_NAME ro.twrp.device.name
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
-
-ifneq ($(TW_SKKK_VER_CODE),)
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.version.skkk.code=$(TW_SKKK_VER_CODE)
-endif
-
-TWRP_REQUIRED_MODULES += miui_prebuilt \
-    magisk_prebuilt
-
-# Vibrator
+# Boot control HAL
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
+    bootctrl.kona \
+    bootctrl.kona.recovery
 
-# Modules
-TARGET_RECOVERY_DEVICE_MODULES += libion vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
+# PRODUCT_PACKAGES += \
 
-#Display
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+#     bootctrl.kona \
+#     libgptutils \
+#     libz \
+#     libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Qcom decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+	android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# Soong Namespaces : Qcom commonsys Display
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys-intf/display
+
+# Recovery libs
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
+    # libdisplayconfig.qti
+
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
+    # $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
 
-# VINTF
-PRODUCT_ENFORCE_VINTF_MANIFEST := true
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+BOARD_USES_METADATA_PARTITION := true
+
+# Platform 
+PLATFORM_VERSION := 99.87.36
+PLATFORM_SECURITY_PATCH := 2127-12-31
+PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
+
+# AIDL Vibrator
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS_FQNAME := "IVibrator/vibratorfeature"
